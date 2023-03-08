29a3f20a9a29f18c7174b9b9fd3b046f6f4243c4
shynigeorge <112165304+shynigeorge@users.noreply.github.com>
2023-03-08 14:32:14 +0530

    Update urls.py

--- bankapp/urls.py
+++ bankapp/urls.py
@@ -2,14 +2,13 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
+     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('demo/', views.demo, name='demo'),
     path('registration/', views.registration, name='registration'),
-
-    path('person_change/<int:pk>/', views.person_update_view, name='person_change'),
+    path('<int:pk>/', views.person_update_view, name='person_change'),
     path('ajax/load-branch/', views.load_branch, name='ajax_load_branch'),
+    path('demo/', views.demo, name='demo'),
 
 ]
