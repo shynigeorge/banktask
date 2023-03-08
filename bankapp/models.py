7be492b3f83ccbc381ebe6ee3118a743cc86a255
shynigeorge <112165304+shynigeorge@users.noreply.github.com>
2023-03-08 14:21:37 +0530

    Update models.py

--- bankapp/models.py
+++ bankapp/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 
 # Create your models here.
-from django.db.models import ForeignKey
+
 
 class District(models.Model):
     name=models.CharField(max_length=800)
     def __str__(self):
         return '{}'.format(self.name)
 
-class Branch(models.Model):
-    branch=models.CharField(max_length=800)
+class City(models.Model):
+    name=models.CharField(max_length=800)
     district = models.ForeignKey(District, on_delete=models.CASCADE)
 
     def __str__(self):
-        return '{}'.format(self.  branch)
+        return '{}'.format(self.name)
 
 class Register(models.Model):
     name = models.CharField(max_length=200)
@@ -25,7 +25,7 @@ class Register(models.Model):
     email = models.EmailField(max_length=200)
     address = models.CharField(max_length=200)
     district=models.ForeignKey(District,on_delete=models.SET_NULL, null=True,blank=True)
-    branch=models.ForeignKey(Branch,on_delete=models.SET_NULL, null=True,blank=True)
+    city=models.ForeignKey(City,on_delete=models.SET_NULL, null=True,blank=True)
     account= models.CharField(max_length=200)
     material = models.CharField(max_length=200)
 
